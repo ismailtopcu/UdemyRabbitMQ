b564db3fcbb5e59d3c6f3507c6fa70168dcd3a0d
ismailtopcu <ismailtopcu.92@gmail.com>
2023-08-10 23:36:24 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://dziltqze:1VhtcPDfsWQNjwddF717AAsrWBfOSsQw@toad.rmq.cloudamqp.com/dziltqze");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+            
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"log{x}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+
+                Console.WriteLine($"Mesaj gönderilmiştir:{message}");
+            });
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyRabbitMQ.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://dziltqze:1VhtcPDfsWQNjwddF717AAsrWBfOSsQw@toad.rmq.cloudamqp.com/dziltqze");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var randomQueueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(randomQueueName,false, consumer);
+            Console.WriteLine("Logları dinleniyor...");
+
+            consumer.Received +=  (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj: " + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+
+                
+            };
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
